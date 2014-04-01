949a60133f9a83843f34ef030246bbbb43c7a522
Christina Carter <cvcarter@gmail.com>
2014-04-01 14:33:53 -0400

    load images

--- js/myPaperScript.js
+++ js/myPaperScript.js
@@ -1,92 +1,122 @@
-// var myPath = new Path();
-// 	myPath.strokeColor = 'black';
-// 	myPath.add(new Point(0, 0));
-// 	myPath.add(new Point(100, 50));
+//load images and animate images
+//get image data from openframeworks
 
-// 	// myPath.insert(2, new Point (200, 50));
-	
-// 	for (i = 0; i<20; i++){
-// 		myPath.insert(1+i, new Point(12*i, 15*i));
-		
-// 	}
-// 	myPath.closed = true;
-
-var width, height, center;
-var points = 10;
-var smooth = true;
-var path = new Path();
-var mousePos = view.center / 2;
-var pathHeight = mousePos.y; // starting path height is half of the view's height
-path.fillColor = 'black';
-initializePath();
-
-function initializePath() { 
-	// The View object wraps an HTML element and 
-	// handles drawing and user interaction through mouse/keyboard
-	center = view.center;
-	width = view.size.width;
-	height = view.size.height / 2;
-	path.segments = [];
-	path.add(view.bounds.bottomLeft); //first point is bottom left
-	for (var i = 1; i < points; i++) { // make 10 points between first and last
-		var point = new Point(width / points * i, center.y);
-		path.add(point);
-	}
-	path.add(view.bounds.bottomRight); //last point is bottom right
-	path.fullySelected = true;
-}
+//------------------------------------------------------
+// basic drawing code
+var myPath;
+var myCircle;
+var circleCount =0;
+var myCircles = [];
+var isPathComplete = true;
+var frame;
+var myImage = 'img2.jpg';
+// var myImgNames = ['img1.jpg', 'img2.jpg'];
 
 
-// function onFrame(event) {
-// 	pathHeight += (center.y - mousePos.y - pathHeight) / 10; //view.center - mouseposition - mouseposition
-// 	for (var i = 1; i < points; i++) {
-// 		var sinSeed = event.count + (i + i % 10) * 100;
-// 		var sinHeight = Math.sin(sinSeed / 200) * pathHeight;
-// 		var yPos = Math.sin(sinSeed / 100) * sinHeight + height;
-// 		path.segments[i].point.y = yPos;
-// 	}
-// 	if (smooth)
-// 		path.smooth();
+// function loadImages(){
+// 	console.log("inside loadImages");
+// // for (i = 0; i<myImgNames; i++){
+// // 	console.log("inside image loop");
+ 	var raster = new Raster('img1.jpg');
+ 	raster.visible = false;
+	raster.position = view.center;
+// 	console.log("images loaded");
+// // }
 // }
 
-function onFrame(event) {
-	var yPos;
-	pathHeight = mousePos.y;
-	for (var i = 1; i < points; i++) {
-		if (i%2 == 0) {
-		yPos = pathHeight + 10*i;
+function Circle(circleNum, circlePos){
+	this.num = circleNum;
+	this.pos = circlePos;
+}
+
+var textItem = new PointText({
+	content: 'Draw a path by clicking the mouse.',
+	point: new Point(20, 30),
+	fillColor: 'black',
+});
+
+function drawFrame(){
+	var width = view.size.width;
+	var height = view.size.height;
+
+	frame = new Path.Rectangle({
+		point: [width/4, height/4],
+		size: [520, 300],
+		strokeColor: 'black',
+		fillColor: 'black'
+	})
+}
+
+function onMouseDown(event) {
+	if (isPathComplete == true && frame.bounds.contains(event.point)){
+		console.log("intersects: " + frame.bounds.contains(event.point))
+		
+		//variable for each x,y of mouseclick and for count of total mouseclicks 
+		var currPoint = event.point;
+		circleCount = circleCount + 1;
+		console.log(circleCount);
+		console.log(currPoint);
+
+		//create circle at every click
+		myCircle = new Path.Circle({
+		center: currPoint,
+		radius: 10
+		});
+		myCircle.fillColor = 'red';
+		
+		//store x,y and count into temp object, push to array of Circle prototype
+		var tempObject = new Circle(circleCount, currPoint);
+		myCircles.push(tempObject);
+		console.log(myCircles);
+
+		//create new layer of myCircle so they can be removed later
+		// var circleLayer = new Layer ({
+		// 	children: [myCircle],
+		// });
 		}
 		else {
-			yPos = pathHeight - 10*i
-		}
-		path.segments[i].point.y = yPos;
+		//code for sending object data to API here
+		
+		console.log("first in else statement: " + isPathComplete);
+		isPathComplete = true;
+		console.log("second in else statement: " + isPathComplete);
 	}
-	if (smooth)
-		path.smooth();
 }
 
 
-function onMouseMove(event) {
-	mousePos = event.point;
+function onMouseDrag(event) {
+
 }
 
-// function onMouseDown(event) {
-// 	smooth = !smooth;
-// 	if (!smooth) {
-// 		// If smooth has been turned off, we need to reset
-// 		// the handles of the path:
-// 		for (var i = 0, l = path.segments.length; i < l; i++) {
-// 			var segment = path.segments[i];
-// 			segment.handleIn = segment.handleOut = null;
-// 		}
-// 	}
+function onMouseUp(event) {
+
+}
+
+$(document).ready(function(){
+	console.log("Loaded!");
+	drawFrame();
+	// loadImages();
+	$('#update').click(function(){
+		isPathComplete = false;
+		console.log("button press: " + isPathComplete);
+		myCircles.length = 0;
+		circleCount = 0;
+		console.log("myCircles should be clear: " + myCircles.length);
+		project.activeLayer.removeChildren([2]);
+		raster.visible = true;
+		// $("#body").append("<img src=" + myImage + "/>");
+		isPathComplete = true;
+	})
+
+
+});
+// //this is another way of doing the path on the onMouseDown function. what's the diff?
+// // Create a new path and set its stroke color to black:
+// path = new Path({
+// 	segments: [event.point],
+// 	strokeColor: 'black',
+// 	// Select the path, so we can see its segment points:
+// 	fullySelected: true
+// });
 // }
 
-function onMouseDown(event) {	
-	
-}
-
-// Reposition the path whenever the window is resized:
-function onResize(event) {
-	initializePath();
-}
